984ec7a0e7c290e817a060ce136eb52acb6a51a6
Hansybx <hansybx@outlook.com>
2023-03-16 22:01:34 +0800

    feat 命令行参数校验

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use clap::{command, Parser, Subcommand, Args};
+use std::str::FromStr;
 
-#[derive(Parser,Debug)]
+use anyhow::{Ok, Result,anyhow};
+use clap::{command, Args, Parser, Subcommand};
+use reqwest::Url;
+
+#[derive(Parser, Debug)]
 #[command(name = "Httpie-rs")]
 #[command(author = "Hansybx <hansybx@outlook.com>")]
 #[command(version = "1.0")]
@@ -10,23 +14,58 @@ struct Opts {
     subcmd: SubCommond,
 }
 
-#[derive(Subcommand,Debug)]
+#[derive(Subcommand, Debug)]
 enum SubCommond {
     Get(Get),
     Post(Post),
 }
 
 // get 子命令
-#[derive(Args,Debug)]
+#[derive(Args, Debug)]
 struct Get {
+    #[arg(value_parser = parse_url)]
     url: String,
 }
 
 // post 子命令。需要输入一个 URL，和若干个可选的 key=value，用于提供 json body
-#[derive(Args,Debug)]
+#[derive(Args, Debug)]
 struct Post {
+    #[arg(value_parser = parse_url)]
     url: String,
-    body: Vec<String>,
+    #[arg(value_parser = parse_kv_pair)]
+    body: Vec<KvPair>,
+}
+
+fn parse_url(url: &str) -> Result<String> {
+    let _url: Url = url.parse()?;
+    Ok(url.into())
+}
+
+#[derive(PartialEq,Clone,Debug)]
+struct KvPair {
+    k: String,
+    v: String,
+}
+
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split("=");
+        let err = || anyhow!(format!("Failed to parse {}", s));
+        Ok(Self {
+            // 我们将其转换成 Ok(T)/Err(E)，然后用 ? 处理错误
+            // 从迭代器中取第一个结果作为 key，迭代器返回 Some(T)/None
+            k: (split.next().ok_or_else(err)?).to_string(),
+            // 从迭代器中取第二个结果作为 value
+            v: (split.next().ok_or_else(err)?).to_string(),
+        })
+    }
+}
+
+/// 因为我们为 KvPair 实现了 FromStr，这里可以直接 s.parse() 得到 KvPair
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+    Ok(s.parse()?)
 }
 
 fn main() {
